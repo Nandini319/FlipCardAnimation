3b4304cf3b9273d94edbddc51c04993a9c3fa2a0
Nandini Vithlani <nandinivithlani99@gmail.com>
2023-07-25 14:15:54 +0530

    Flip Card Animation

--- FlipCardAnimation/ContentView.swift
+++ FlipCardAnimation/ContentView.swift
@@ -8,17 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isFlipped = false
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            if isFlipped {
+                CardBack()
+            } else {
+                CardFront()
+            }
+        }
+        .onTapGesture {
+            withAnimation(.easeInOut(duration: 0.5)) {
+                self.isFlipped.toggle()
+            }
         }
         .padding()
+        .rotation3DEffect(.degrees(isFlipped ? 180 : 0), axis: (x: 1, y: 0, z: 0))
     }
 }
 
+struct CardFront: View {
+    var body: some View {
+        ZStack {
+            Image("CardFront")
+                .foregroundColor(.white)
+        }
+    }
+}
+
+struct CardBack: View {
+    var body: some View {
+        ZStack {
+            Image("CardBack")
+                .foregroundColor(.white)
+        }
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
