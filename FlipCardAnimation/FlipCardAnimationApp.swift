3d10a7f8d15145507d5f3c1fd67b90aed487ec47
Nandini Vithlani <nandinivithlani99@gmail.com>
2023-07-25 14:08:08 +0530

    Initial Commit

---
+++ FlipCardAnimation/FlipCardAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipCardAnimationApp.swift
+//  FlipCardAnimation
+//
+//  Created by Nandini Vithlani on 25/07/23.
+//
+
+import SwiftUI
+
+@main
+struct FlipCardAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
